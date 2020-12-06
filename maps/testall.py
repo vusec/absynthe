7b1f479d00e3f0aa578d443fa8fe1b83d9294adf
Ben Gras <beng@shrike-systems.com>
2020-12-06 23:55:26 +0100

    load check moved to acquire

--- maps/testall.py
+++ maps/testall.py
@@ -445,13 +445,6 @@ if __name__ == "__main__":
     killall='/usr/bin/killall'
     devnull=open('/dev/null', 'w')
     load1, load5, laod15 = os.getloadavg()
-    limit=0.6
-    if load1 > limit:
-        print('Please run me only on an idle system.')
-        print('Found 1 minute load average of %.1f' % load1)
-        print('Expecting max 1 minute load average of %.1f' % limit)
-        print('This is protection against old measurement processes\nlingering and corrupting new measurements.')
-        sys.exit(1)
     if not os.path.exists(killall):
         raise Exception('need killall binary')
     if len(sys.argv) < 2:
@@ -465,6 +458,13 @@ if __name__ == "__main__":
         uarch=sys.argv[3]
         num_measurements=int(sys.argv[4])
         n_instructions=int(sys.argv[5])
+        limit=0.6
+        if load1 > limit:
+            print('Please run me only on an idle system.')
+            print('Found 1 minute load average of %.1f' % load1)
+            print('Expecting max 1 minute load average of %.1f' % limit)
+            print('This is protection against old measurement processes\nlingering and corrupting new measurements.')
+            sys.exit(1)
         acquire(exe, uarch, num_measurements, progressbar, n_instructions)
     if sys.argv[1] == 'aggregate':
         aggregate(progressbar)
